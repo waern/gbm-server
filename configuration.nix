422d3050a24fe0cc9504faba5aa85a0aff65189d
David Waern <david.waern@gmail.com>
2015-12-23 11:09:52 +0100

    Set environment variable

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,10 @@ let gbm-server = pkgs.haskellPackages.callPackage ./default.nix {}; in
   networking.firewall.allowedTCPPorts = [ 443 ];
 
   systemd.services.gbm-server = {
+    environment = {
+      GBM_SERVER_THING_DIR = "/root/gbm-server/things";
+    };
+
     serviceConfig = {
       ExecStart = "${gbm-server}/bin/gbm-server";
     };
